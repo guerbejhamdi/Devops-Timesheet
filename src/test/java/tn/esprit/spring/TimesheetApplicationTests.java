4a66fbab8a9942125b1d28a1c5906f541448acf6
besbes chaima <chaima.besbes2@gmail.com>
2021-10-13 02:16:49 +0100

    test of service getdepartement with success

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -1,13 +1,30 @@
 package tn.esprit.spring;
 
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.services.DepartementServiceImpl;
 
 @SpringBootTest
 class TimesheetApplicationTests {
+	
+	@Autowired
+	DepartementServiceImpl departSer ;
+
 
 	@Test
 	void contextLoads() {
 	}
+	
+   	@Test
+	public void testGetDepartement() {
+	assertEquals(1, departSer.getAllDepartements().size());
 
+}
 }
