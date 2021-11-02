f9a388d705d0935ab40ffb51e7314b8e599068d0
Hamdi Guerbej <hamdi.guerbej@esprit.tn>
2021-11-03 00:28:45 +0100

    if-then-else statement code smell fix

--- src/main/java/tn/esprit/spring/entities/TimesheetPK.java
+++ src/main/java/tn/esprit/spring/entities/TimesheetPK.java
@@ -72,9 +72,10 @@ public class TimesheetPK implements Serializable {
 			return false;}
 		if (idEmploye != other.idEmploye)
 			return false;
-		if (idMission != other.idMission)
-			return false;
-		return true;
+		
+		return idMission == other.idMission;
+
+		
 	}
 
 	public void setIdMission(int idMission) {
